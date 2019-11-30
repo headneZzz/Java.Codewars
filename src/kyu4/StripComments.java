b080f3ea66dec12ee8b804404aa417d11256baab
headneZzz <mr.darkilya@mail.ru>
2019-11-30 22:35:52 +0300

    Add new

--- src/kyu4/StripComments.java
+++ src/kyu4/StripComments.java
@@ -2,13 +2,6 @@ package kyu4;
 
 public class StripComments {
     public static String stripComments(String text, String[] commentSymbols) {
-        StringBuilder newtext = new StringBuilder(text);
-        for (int i = 0; i < newtext.length() - 1; i++) {
-            if (newtext.charAt(i) == '\\' && newtext.charAt(i + 1) == 'n') {
-                newtext.replace(i,i+2,"\n");
-            }
-        }
-        text = newtext.toString();
         StringBuilder out = new StringBuilder();
         char[] charsText = text.toCharArray();
         char[] comments = new char[commentSymbols.length];
@@ -18,32 +11,28 @@ public class StripComments {
             j++;
         }
         boolean isComment = false;
-        for (int i = 0; i < charsText.length; i++) {
+        for (char c : charsText) {
             if (!isComment) {
                 for (char s : comments) {
-                    if (charsText[i] == s) {
+                    if (c == s) {
                         isComment = true;
                         break;
                     }
                 }
-                if (!isComment && charsText[i] != '\n')
-                    out.append(charsText[i]);
+                if (!isComment && c != '\n')
+                    out.append(c);
             }
-            if (charsText[i] == '\n') {
-                while (out.charAt(out.length() - 1) == ' ') {
+            if (c == '\n') {
+                while (out.length() != 0 && out.charAt(out.length() - 1) == ' ') {
                     out.deleteCharAt(out.length() - 1);
                 }
                 isComment = false;
-                out.append(charsText[i]);
+                out.append(c);
             }
         }
         while (out.length() != 0 && out.charAt(out.length() - 1) == ' ') {
             out.deleteCharAt(out.length() - 1);
         }
         return out.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.print(stripComments("e\\n\\nf\\n\\neb\\n\\nc\\n\\nc\\n\\nf\\n\\nedab\\n\\nfc\\n\\nd\\n\\n!\\n\\n!\\n\\ne\\n\\n!!e\\n\\nf\\n\\nf\\n\\na\\n\\n!d\\n\\nb\\n\\nd\\n\\ncef\\n\\ne\\n\\n\\n\\n\\nb\\n\\n\\n\\n\\nbf\\n\\nfa\\n\\n!bbb\\n\\nc\\n\\nc\\n\\nfd\\n\\n!c\\n\\ne\\n\\na\\n\\n\\n\\n\\nd\\n\\nd\\n\\nf\\n\\n\\n\\n\\nd\\n\\n!\\n\\nc\\n\\ndaaf\\n\\nc\\n\\nc\\n\\n\\na\\n\\nbb\\n\\nff!\\n\\n!d\\n!ae!c\\n\\nc\\n\\nf\\n\\nee\\n\\nc\\n\\n\\n\\ndca\\n\\nc\\n\\nde\\n\\ne\\n\\nb\\n\\nd\\n\\nad\\n\\n\\neda\\n\\nb\\n\\na\\n\\n\\n\\n\\n\\n\\n\\na\\n\\nd\\n\\ne!f\\n\\na\\n\\nfcfa\\n\\nf", new String[]{"#", "$", "!", "-"}));
     }
 }
